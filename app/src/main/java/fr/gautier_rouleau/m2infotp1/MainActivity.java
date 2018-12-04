27184dee99504f27299e0021941d3e124eb11bf1
Gautier Rouleau <gautier.rouleau@gmail.com>
2018-12-04 15:57:30 +0100

    added menu item to reset inputs

--- app/src/main/java/fr/gautier_rouleau/m2infotp1/MainActivity.java
+++ app/src/main/java/fr/gautier_rouleau/m2infotp1/MainActivity.java
@@ -2,6 +2,10 @@ package fr.gautier_rouleau.m2infotp1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -30,5 +34,36 @@ public class MainActivity extends AppCompatActivity {
                 firstName.getText(), lastName.getText(), dateOfBirth.getText(), cityOfBirth.getText());
 
         Toast.makeText(this, greetings, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.resetIHM:
+                resetIHM();
+                break;
+            default:
+                Toast.makeText(this, "Not implemented yet.", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return true;
+    }
+
+    public void resetIHM() {
+        this.firstName.setText(""); this.lastName.setText("");
+        this.dateOfBirth.setText(""); this.cityOfBirth.setText("");
     }
 }
