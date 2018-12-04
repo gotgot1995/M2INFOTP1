89ae8064681dc19cc1f0be08069d08f605147d91
Gautier Rouleau <gautier.rouleau@gmail.com>
2018-12-04 15:21:59 +0100

    added betterknife library, rearanged XML layout file

--- app/src/main/java/fr/gautier_rouleau/m2infotp1/MainActivity.java
+++ app/src/main/java/fr/gautier_rouleau/m2infotp1/MainActivity.java
@@ -2,12 +2,19 @@ package fr.gautier_rouleau.m2infotp1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitPersonalInfo(View view) {
+        Toast.makeText(this, "Button works !", Toast.LENGTH_SHORT).show();
     }
 }
