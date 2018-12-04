c490406afc0400910f671620a84ed7daf2e7e0df
Gautier Rouleau <gautier.rouleau@gmail.com>
2018-12-04 15:35:04 +0100

    removed butterknife (only compatible with API >= 24), added view bindings and Toast message

--- app/src/main/java/fr/gautier_rouleau/m2infotp1/MainActivity.java
+++ app/src/main/java/fr/gautier_rouleau/m2infotp1/MainActivity.java
@@ -1,20 +1,34 @@
 package fr.gautier_rouleau.m2infotp1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private
+    private EditText firstName;
+
+    private EditText lastName;
+
+    private EditText dateOfBirth;
+
+    private EditText cityOfBirth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.firstName = findViewById(R.id.firstName);
+        this.lastName = findViewById(R.id.lastName);
+        this.dateOfBirth = findViewById(R.id.dateOfBirth);
+        this.cityOfBirth = findViewById(R.id.cityOfBirth);
     }
 
     public void submitPersonalInfo(View view) {
-        Toast.makeText(this, "Button works !", Toast.LENGTH_SHORT).show();
+        String greetings = String.format("Hello %s %s, you were born on %s in %s",
+                firstName.getText(), lastName.getText(), dateOfBirth.getText(), cityOfBirth.getText());
+
+        Toast.makeText(this, greetings, Toast.LENGTH_LONG).show();
     }
 }
